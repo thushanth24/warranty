55cd6b48c14155f6dc23a8989f00ad79ab441cbf
Thushanth <116872194+thushanth24@users.noreply.github.com>
2025-07-27 21:05:17 +0530

    fix : deploy errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
@@ -11,6 +13,18 @@ export default defineConfig({
   preview: {
     port: 5173,
     strictPort: true,
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
+      },
+      {
+        find: '@shared',
+        replacement: path.resolve(__dirname, 'shared'),
+      },
+    ],
   },
   build: {
     outDir: 'dist',
